ae1eb8843f89daa4dffc3262789e02017dce91c4
AurelieT <aurelie.tudare@gmail.com>
2018-10-03 12:06:45 +0200

    ajouté code du formulaire, première passe

---
+++ formulaire.php
@@ -0,0 +1,13 @@
+<form action="formulaire.php" method="post">
+    <div>
+        <label for="author">Auteur</label><br />
+        <input type="text" id="author" name="author" />
+    </div>
+    <div>
+        <label for="comment">Commentaire</label><br />
+        <textarea id="comment" name="comment"></textarea>
+    </div>
+    <div>
+        <input type="submit" />
+    </div>
+</form>
