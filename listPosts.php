718311ae5a2701efe04ce68068c02a2e8d98b4ed
AurelieT <aurelie.tudare@gmail.com>
2018-10-03 12:15:58 +0200

    ajouté le code pour faire apparaitre le listing des posts

---
+++ listPosts.php
@@ -0,0 +1,35 @@
+?php $title = 'Mon blog';?>
+
+<?php ob_start(); ?>
+<h1>Mon super blog !</h1>
+<p><h2>Derniers billets du blog :</h2></p>
+ 
+
+<?php
+while ($data = $posts->fetch())
+{
+?>
+<div class="news">
+    <h3>
+        <?php echo htmlspecialchars($data['title']); ?>
+        <em>le <?php echo htmlspecialchars($data['creation_date_fr']); ?></em>
+    </h3>
+    
+    <p>
+    <?php
+    // On affiche le contenu du billet
+    echo nl2br(htmlspecialchars($data['content']));
+    ?>
+    <br />
+    <em><a href="index.php?action=post&id=<?php echo $data['id']; ?>">Commentaires</a></em>
+    </p>
+</div>
+
+<?php
+} // Fin de la boucle des billets
+$posts->closeCursor();
+?>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
